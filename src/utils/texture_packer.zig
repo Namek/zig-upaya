5927368ebf5090781ecc8285ef97e934b96f4404
foxnne <foxnne@gmail.com>
2021-09-17 16:29:55 -0500

    save heightmap

--- src/utils/texture_packer.zig
+++ src/utils/texture_packer.zig
@@ -82,13 +82,14 @@ pub const TexturePacker = struct {
         /// saves the atlas image and a json file with the atlas details. filename should be only the name with no extension.
         pub fn save(self: Atlas, folder: []const u8, filename: []const u8) void {
             const img_filename = std.mem.concat(upaya.mem.allocator, u8, &[_][]const u8{ filename, ".png" }) catch unreachable;
+            const heightmap_filename = std.mem.concat(upaya.mem.allocator, u8, &[_][]const u8{ filename, "_h.png" }) catch unreachable;
             const atlas_filename = std.mem.concat(upaya.mem.allocator, u8, &[_][]const u8{ filename, ".atlas" }) catch unreachable;
 
             var out_file = fs.path.join(upaya.mem.tmp_allocator, &[_][]const u8{ folder, img_filename }) catch unreachable;
             self.image.save(out_file);
 
-            // out_file = fs.path.join(upaya.mem.tmp_allocator, &[_][]const u8{ folder, heightmap_filename }) catch unreachable;
-            // self.heightmap.save(out_file);
+            out_file = fs.path.join(upaya.mem.tmp_allocator, &[_][]const u8{ folder, heightmap_filename }) catch unreachable;
+            self.heightmap.save(out_file);
 
             out_file = fs.path.join(upaya.mem.tmp_allocator, &[_][]const u8{ folder, atlas_filename }) catch unreachable;
             var handle = std.fs.cwd().createFile(out_file, .{}) catch unreachable;
