6cd658f5903df8ee902ab1739a44f326d58786e3
foxnne <foxnne@gmail.com>
2022-01-09 13:46:06 -0600

    remove old windows hack

--- src/deps/zip/build.zig
+++ src/deps/zip/build.zig
@@ -8,7 +8,7 @@ pub fn linkArtifact(b: *Builder, exe: *std.build.LibExeObjStep, target: std.zig.
     exe.linkLibC();
     
     exe.addIncludeDir(prefix_path ++ "src/deps/zip/zip/src");
-    const c_flags = if (builtin.target.os.tag == .macos) [_][]const u8{ "-std=c99", "-ObjC", "-fobjc-arc" } else [_][]const u8{"-std=c99", "-fno-sanitize=undefined", "-D_ftelli64=ftello64", "-D_fseeki64=fseeko64" };
+    const c_flags = if (builtin.target.os.tag == .macos) [_][]const u8{ "-std=c99", "-ObjC", "-fobjc-arc" } else [_][]const u8{"-std=c99", "-fno-sanitize=undefined" };
     exe.addCSourceFile(prefix_path ++ "src/deps/zip/zip/src/zip.c", &c_flags);
 }
 
